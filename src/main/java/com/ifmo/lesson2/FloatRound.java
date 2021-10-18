de1893377e4f4450bbf5a949cb050cdc6b89cffa
Armidzy <sasha.ilina@gmail.com>
2021-10-18 21:37:12 +0300

    floatRound

--- src/main/java/com/ifmo/lesson2/FloatRound.java
+++ src/main/java/com/ifmo/lesson2/FloatRound.java
@@ -6,7 +6,7 @@ public class FloatRound {
     Создайте программу, округляющую число n до ближайшего целого и выводящую результат на экран.
      */
     public static void main(String[] args) {
-        float n = 12.4F;
+        float n = -12.5F;
 
         float rounded = round(n);
 
@@ -15,13 +15,22 @@ public class FloatRound {
 
     public static float round(float n) {
         float remainder = n % 1;
+        if (remainder < 0)
+            remainder *= -1;
         int rounded;
         //System.out.println(remainder);
-        if (remainder >= 0.5F)
-            rounded=(int)n+1;
-        else
-            rounded = (int)n;
-
+        if (n>0) {
+            if (remainder >= 0.5F)
+                rounded = (int) n + 1;
+            else
+                rounded = (int) n;
+        }
+        else{
+            if (remainder >= 0.5F)
+                rounded = (int) n - 1;
+            else
+                rounded = (int) n;
+        }
         return rounded;
     }
 }
