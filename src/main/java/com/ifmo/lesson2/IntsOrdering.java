6fcc41d24ea9c205f9466e99f0de7ca0bdd82775
Armidzy <sasha.ilina@gmail.com>
2021-10-18 21:47:14 +0300

    intsord

--- src/main/java/com/ifmo/lesson2/IntsOrdering.java
+++ src/main/java/com/ifmo/lesson2/IntsOrdering.java
@@ -26,8 +26,26 @@ public class IntsOrdering {
 
     public static String ordering(int a, int b, int c) {
         // TODO implement
+        int mas[]= {a, b, c};
+        int help;
+
+        for (int i = 1; i < 3; i++) {  //ищем минимум и записываем его в 1 эл
+            if (mas[i]<mas[0]) {
+                help=mas[0];
+                mas[0] = mas[i];
+                mas[i]=help;
+
+            }
+        }
+        for (int i = 0; i < 2; i++) {  //ищем максимум и записываем его в 3 эл
+            if (mas[i]>mas[2]) {
+                help=mas[2];
+                mas[2] = mas[i];
+                mas[i]=help;
+            }
+        }
 
         return "Числа в переменных a, b и c: " + a + ", " + b + ", " + c + "\n" +
-                "Возрастающая последовательность: " + 0 + ", " + 0 + ", " + 0;
+                "Возрастающая последовательность: " + mas[0] + ", " + mas[1] + ", " + mas[2];
     }
 }
